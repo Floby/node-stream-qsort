0bd6118332e1bf6d3a225d4665d0939c438057e3
Florent Jaby <fjaby@octo.com>
2013-04-09 15:52:06 +0200

    add test for compare as options

--- test/test-sort-custom-compare.js
+++ test/test-sort-custom-compare.js
@@ -27,3 +27,27 @@ exports.testSortStringsLowercase = function(test) {
 
 
 
+exports.testSortStringsLowercaseAsOptions = function(test) {
+    var source = ['disney', 'world', 'a', 'aba', 'hello', '8', 'CAPITAL'];
+
+    var compare = function CustomCompare (a, b) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        return a.localeCompare(b);
+    }
+    var s = qsort({compare: compare});
+
+    as(source).pipe(s).pipe(sink({objectMode: true})).on('data', function(data) {
+        clearTimeout(to);
+        test.equal(JSON.stringify(data), JSON.stringify(source.sort(compare)), 'data should be identical');
+        test.done();
+    });
+
+    var to = setTimeout(function() {
+        test.fail('too long');
+        test.done();
+    }, 100);
+}
+
+
+
